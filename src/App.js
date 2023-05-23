2e73d944447d78ed72e61cbe6de45cec6f11dbd4
Adwaith Rajesh <64030486+Adwaith123@users.noreply.github.com>
2023-05-23 20:26:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 // This is our api key to esatablish connection to the clarifai
 const app = new Clarifai.App({
-  apiKey: 'API_CLARIFAI'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const particlesOptions = {
